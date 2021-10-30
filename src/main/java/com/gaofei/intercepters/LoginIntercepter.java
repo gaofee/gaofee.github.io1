88e341b154cb351a38b3fc2ef4287c66f08a50fe
feege <79527743@qq.com>
2021-10-30 10:02:34 +0800

    222

--- src/main/java/com/gaofei/intercepters/LoginIntercepter.java
+++ src/main/java/com/gaofei/intercepters/LoginIntercepter.java
@@ -51,7 +51,7 @@ public class LoginIntercepter implements HandlerInterceptor {
                 res.setHeader("Access-Control-Allow-Origin", "*");
                 res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
                 res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Credentials", "true");//
                 out.write(info);
 
 
